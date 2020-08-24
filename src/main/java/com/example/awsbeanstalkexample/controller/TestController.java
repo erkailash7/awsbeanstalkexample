96b5ba895afa2fe66deba012a0d3d8c092b68385
Kailash <kailashsaini7@gmail.com>
2020-08-24 16:45:46 +0530

    trantor commit

--- src/main/java/com/example/awsbeanstalkexample/controller/TestController.java
+++ src/main/java/com/example/awsbeanstalkexample/controller/TestController.java
@@ -9,7 +9,7 @@ public class TestController {
 
     @GetMapping("/message")
     public String getMessage(){
-        String msg = "Spring boot application deployed on beanstalk version-16";
+        String msg = "Spring boot application deployed on beanstalk Trantor depoloy";
         return msg;
     }
 }
